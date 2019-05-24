578ac88f85a4023e365869a41f97adaed527d121
vernar <arbuzov.mail@gmail.com>
2019-05-24 14:33:06 +0300

    3. Модальные окна popup-consultation

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -148,14 +148,21 @@ module.exports = ModalWindow;
 
 let ModalWindow = __webpack_require__(/*! ./ModalWindow.js */ "./src/ModalWindow.js")
 ;
-let modal = new ModalWindow(
-    document.querySelector('.popup-dialog'),
+let modalDialog = new ModalWindow(
+    document.querySelector('.popup-design .popup-dialog'),
     document.querySelector('.popup-design'),
-    document.querySelectorAll('.button-order'),
+    document.querySelectorAll('.button-design'),
     [
         document.querySelector('.popup-design .popup-close'),
-    ]
-);
+    ]),
+    modalConsultation = new ModalWindow(
+    document.querySelector('.popup-consultation .popup-dialog'),
+    document.querySelector('.popup-consultation'),
+    document.querySelectorAll('.button-consultation'),
+    [
+        document.querySelector('.popup-consultation .popup-close'),
+    ])
+;
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@
 
 let ModalWindow = require('./ModalWindow.js')
 ;
-let modal = new ModalWindow(
-    document.querySelector('.popup-dialog'),
+let modalDialog = new ModalWindow(
+    document.querySelector('.popup-design .popup-dialog'),
     document.querySelector('.popup-design'),
-    document.querySelectorAll('.button-order'),
+    document.querySelectorAll('.button-design'),
     [
         document.querySelector('.popup-design .popup-close'),
-    ]
-);
+    ]),
+    modalConsultation = new ModalWindow(
+    document.querySelector('.popup-consultation .popup-dialog'),
+    document.querySelector('.popup-consultation'),
+    document.querySelectorAll('.button-consultation'),
+    [
+        document.querySelector('.popup-consultation .popup-close'),
+    ])
+;
